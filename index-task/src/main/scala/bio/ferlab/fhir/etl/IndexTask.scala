07378274ad56cf464e361414e88251c8a0bc47f6
adrian <adipaul1981@yahoo.ca>
2023-02-28 19:56:43 -0500

    test

--- index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
+++ index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
@@ -41,7 +41,7 @@ object IndexTask extends App {
     .appName(s"IndexTask")
     .getOrCreate()
 
-  conf.sparkconf.keys.foreach(println)
+  println(conf.sparkconf("spark.hadoop.fs.s3a.access.key").take(3))
 
   val testFile = spark.read.parquet("s3a://cqdg-prod-app-clinical-data-service/es_index/fhir/study_centric/study_id=ST0000017/release_id=7/")
   testFile.show(3, false)
