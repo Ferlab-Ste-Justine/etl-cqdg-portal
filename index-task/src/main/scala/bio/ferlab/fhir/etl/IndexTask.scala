cadd52eca3366141652a23099cdbe597b5021918
adrian <adipaul1981@yahoo.ca>
2023-03-02 15:43:19 -0500

    test

--- index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
+++ index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
@@ -3,9 +3,6 @@ package bio.ferlab.fhir.etl
 import bio.ferlab.datalake.commons.config.{Configuration, ConfigurationLoader, DatasetConf, SimpleConfiguration}
 import bio.ferlab.datalake.spark3.elasticsearch.{ElasticSearchClient, Indexer}
 import bio.ferlab.datalake.spark3.implicits.DatasetConfImplicits.DatasetConfOperations
-import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.regions.Regions
 import software.amazon.awssdk.http.apache.ApacheHttpClient
 import org.apache.spark.SparkConf
 import software.amazon.awssdk.services.s3.{S3Client, S3Configuration}
@@ -18,7 +15,6 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
 
 import java.net.URI
-import scala.io.Source
 //Required import
 import pureconfig.module.enum._
 
@@ -48,29 +44,28 @@ object IndexTask extends App {
     .appName(s"IndexTask")
     .getOrCreate()
 
-//  spark.sparkContext.setLogLevel("ERROR")
-//
-//  val templatePath = s"${conf.storages.find(_.id == "storage").get.path}/templates/template_$jobType.json"
-//
-//  implicit val esClient: ElasticSearchClient =
-//    new ElasticSearchClient(s"$esUrl:$esPort", None, None)
+  spark.sparkContext.setLogLevel("ERROR")
 
-//  val ds: DatasetConf = jobType.toLowerCase match {
-//    case "study_centric" => conf.getDataset("es_index_study_centric")
-//    case "participant_centric" => conf.getDataset("es_index_participant_centric")
-//    case "file_centric" => conf.getDataset("es_index_file_centric")
-//    case "biospecimen_centric" => conf.getDataset("es_index_biospecimen_centric")
-//  }
+  val templatePath = s"${conf.storages.find(_.id == "storage").get.path}/templates/template_$jobType.json"
+
+  implicit val esClient: ElasticSearchClient =
+    new ElasticSearchClient(s"$esUrl:$esPort", None, None)
+
+  val ds: DatasetConf = jobType.toLowerCase match {
+    case "study_centric" => conf.getDataset("es_index_study_centric")
+    case "participant_centric" => conf.getDataset("es_index_participant_centric")
+    case "file_centric" => conf.getDataset("es_index_file_centric")
+    case "biospecimen_centric" => conf.getDataset("es_index_biospecimen_centric")
+  }
 
   val studyList = study_ids.split(",")
 
-//  spark.sparkContext.getConf.getAll.filterNot(c => c._1 == "spark.hadoop.fs.s3a.access.key" || c._1 =="spark.hadoop.fs.s3a.secret.key")
-//    .foreach(e => println(s"${e._1} -> ${e._2}"))
+  //----------------------DELETE-----------------------------------
+  spark.sparkContext.getConf.getAll
+    .filterNot(c => c._1 == "spark.hadoop.fs.s3a.access.key" || c._1 =="spark.hadoop.fs.s3a.secret.key")
+    .foreach(e => println(s"${e._1} -> ${e._2}"))
 
-//  spark.sparkContext.getConf.getAll
-//    .filterNot(c => c._1 == "spark.hadoop.fs.s3a.access.key" || c._1 =="spark.hadoop.fs.s3a.secret.key")
-//    .foreach(e => println(s"${e._1} -> ${e._2.take(3)}"))
-//
+
   val access = spark.sparkContext.getConf.getAll.filter(c => c._1 == "spark.hadoop.fs.s3a.access.key").head._2
   val secret = spark.sparkContext.getConf.getAll.filter(c => c._1 == "spark.hadoop.fs.s3a.secret.key").head._2
 
@@ -100,25 +95,25 @@ object IndexTask extends App {
   val obj = s3.getObject(objectRequest)
 
   println(s3.listBuckets())
-
   val rest = obj.response()
   println(rest)
+  //---------------------------------------------------------
 
-//  studyList.foreach(studyId => {
-//    val indexName = s"${jobType}_${studyId}_$release_id".toLowerCase
-//
-//    println(s"Run Index Task to fill index $indexName")
-//
-//    println("sleep 5min")
-//    Thread.sleep(300000)
-//    println("end sleep")
-//
-//    val df: DataFrame = ds.read
-//      .where(col("release_id") === release_id)
-//      .where(col("study_id") === studyId)
-//
-//    new Indexer("index", templatePath, indexName)
-//      .run(df)
-//  })
+  studyList.foreach(studyId => {
+    val indexName = s"${jobType}_${studyId}_$release_id".toLowerCase
+
+    println(s"Run Index Task to fill index $indexName")
+
+    println("sleep 5min")
+    Thread.sleep(300000)
+    println("end sleep")
+
+    val df: DataFrame = ds.read
+      .where(col("release_id") === release_id)
+      .where(col("study_id") === studyId)
+
+    new Indexer("index", templatePath, indexName)
+      .run(df)
+  })
 
 }
