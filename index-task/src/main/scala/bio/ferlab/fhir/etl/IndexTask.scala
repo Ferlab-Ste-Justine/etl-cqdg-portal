b0bedf79d29e0862c1125a05fb910548057c492c
adrian <adipaul1981@yahoo.ca>
2023-03-22 13:20:39 -0400

    fix/CQDG_138_clean_index_task

--- index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
+++ index-task/src/main/scala/bio/ferlab/fhir/etl/IndexTask.scala
@@ -3,12 +3,10 @@ package bio.ferlab.fhir.etl
 import bio.ferlab.datalake.commons.config.{Configuration, ConfigurationLoader, DatasetConf, SimpleConfiguration}
 import bio.ferlab.datalake.spark3.elasticsearch.{ElasticSearchClient, Indexer}
 import bio.ferlab.datalake.spark3.implicits.DatasetConfImplicits.DatasetConfOperations
-import org.apache.hadoop.shaded.org.apache.http.util.EntityUtils
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import pureconfig.generic.auto._
-import org.elasticsearch.spark.sql._
 //Required import
 import pureconfig.module.enum._
 
@@ -23,8 +21,7 @@ object IndexTask extends App {
   env,            // qa/dev/prd
   project,        // cqdg
   esUrl,
-  esPort,
-  test
+  esPort
   ) = args
 
   val esConfigs = Map(
@@ -38,13 +35,9 @@ object IndexTask extends App {
     "spark.es.nodes.wan.only" -> "true",
     "es.port" -> esPort) //9200
 
-  println(esUrl)
-  println(esPort)
-
   implicit val conf: Configuration = ConfigurationLoader.loadFromResources[SimpleConfiguration](s"config/$env-$project.conf")
 
   val sparkConfigs: SparkConf =
-//    (conf.sparkconf + ("es.nodes" -> s"$esUrl:$esPort"))
     (conf.sparkconf ++ esConfigs)
       .foldLeft(new SparkConf()){ case (c, (k, v)) => c.set(k, v) }
 
@@ -63,13 +56,6 @@ object IndexTask extends App {
 
   spark.sparkContext.getConf.getAll.filterNot(e => e._1 == "spark.hadoop.fs.s3a.access.key" || e._1 == "spark.hadoop.fs.s3a.secret.key")
     .foreach(r => println(s"${r._1} -> ${r._2}"))
-
-  if(test.toLowerCase() == "true"){
-    println("toto")
-    println(EntityUtils.toString(esClient.getIndex("arranger-projects").getEntity))
-    Thread.sleep(300000)
-    println("toto")
-  }
 
   val ds: DatasetConf = jobType.toLowerCase match {
     case "study_centric" => conf.getDataset("es_index_study_centric")
