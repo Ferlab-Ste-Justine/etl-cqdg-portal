6b9274e669e25615f287153c8add6608c99d352b
Adrian <adipaul1981@yahoo.ca>
2023-10-06 15:11:48 -0400

    fix: CQDG-00 update repartition for normalized_snv

--- config/src/main/scala/bio/ferlab/fhir/etl/config/ConfigurationGenerator.scala
+++ config/src/main/scala/bio/ferlab/fhir/etl/config/ConfigurationGenerator.scala
@@ -109,7 +109,7 @@ object ConfigurationGenerator extends App {
         table = Some(TableConf("database", "normalized_snv")),
         partitionby = List("study_id", "dataset", "chromosome"),
         writeoptions = WriteOptions.DEFAULT_OPTIONS ++ Map("overwriteSchema" -> "true"),
-        repartition = Some(RepartitionByColumns(Seq("chromosome"), Some(100)))
+        repartition = Some(RepartitionByRange(Seq("chromosome", "start"), Some(100)))
       ),
       DatasetConf(
         id = "enriched_specimen",
