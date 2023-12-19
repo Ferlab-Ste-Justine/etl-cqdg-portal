39920598828450a86c3d6578d56de4fb16b04eb2
Laura Bégin <lbegin@ferlab.bio>
2023-12-19 15:32:03 -0500

    feat: CQDG-502 Separate studies + study_frequencies_wgs in different splits

--- variant-task/src/main/scala/bio/ferlab/etl/enriched/RunEnrichGenomic.scala
+++ variant-task/src/main/scala/bio/ferlab/etl/enriched/RunEnrichGenomic.scala
@@ -2,10 +2,8 @@ package bio.ferlab.etl.enriched
 
 import bio.ferlab.datalake.commons.config.RuntimeETLContext
 import bio.ferlab.datalake.spark3.genomics.enriched.{Consequences, Variants}
-import bio.ferlab.datalake.spark3.genomics.{FirstElement, FrequencySplit, OccurrenceAggregation, SimpleAggregation}
-import bio.ferlab.etl.Constants.columns.{TRANSMISSIONS, TRANSMISSION_MODE}
+import bio.ferlab.datalake.spark3.genomics.{FirstElement, FrequencySplit, SimpleAggregation, SimpleSplit}
 import mainargs.{ParserForMethods, main}
-import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions._
 
 object RunEnrichGenomic {
@@ -26,22 +24,28 @@ object RunEnrichGenomic {
     rc = rc,
     snvDatasetId = "normalized_snv",
     extraAggregations = Seq(collect_set("source") as "sources"),
-    frequencies = Seq(
+    splits = Seq(
+      SimpleSplit(
+        name = "studies",
+        extraSplitBy = Some(col("study_id")),
+        aggregations = Seq(
+          FirstElement("study_code", col("study_code")),
+          SimpleAggregation(name = "zygosity", c = col("zygosity"))
+        )
+      ),
       FrequencySplit(
-        "studies",
-        filter = Some(col("source") === "WGS"), // Only compute frequencies for whole genomes
-        splitBy = Some(col("study_id")),
+        "study_frequencies_wgs",
+        filter = Some(col("source") === "WGS"),
+        extraSplitBy = Some(col("study_id")),
         byAffected = false,
         extraAggregations = Seq(
           FirstElement("study_code", col("study_code")),
-//          SimpleAggregation(name = TRANSMISSIONS, c = col(TRANSMISSION_MODE)), ----> Removed in SNV for performance
-          SimpleAggregation(name = "zygosity", c = col("zygosity"))
+          //          SimpleAggregation(name = TRANSMISSIONS, c = col(TRANSMISSION_MODE)), ----> Removed in SNV for performance
         )
       ),
       FrequencySplit(
         "internal_frequencies_wgs",
         filter = Some(col("source") === "WGS"),
-        splitBy = None,
         byAffected = false
       )))
 
