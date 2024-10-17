c3123966753d3df258733363fd59008b7c483c51
Adrian <adipaul1981@yahoo.ca>
2024-10-17 16:50:43 -0400

    fix: CQDG-00 test

--- prepare-index/src/main/scala/bio/ferlab/fhir/etl/centricTypes/SimpleParticipant.scala
+++ prepare-index/src/main/scala/bio/ferlab/fhir/etl/centricTypes/SimpleParticipant.scala
@@ -29,13 +29,13 @@ class SimpleParticipant(studyIds: List[String])(implicit configuration: Configur
   override def extract(lastRunDateTime: LocalDateTime = minDateTime,
                        currentRunDateTime: LocalDateTime = LocalDateTime.now())(implicit spark: SparkSession): Map[String, DataFrame] = {
     (Seq(
-      normalized_phenotype, normalized_disease, normalized_disease_status, normalized_cause_of_death,
+      normalized_patient, normalized_phenotype, normalized_disease, normalized_disease_status, normalized_cause_of_death,
       normalized_group, normalized_family_relationship, normalized_researchstudy)
       .map(ds => ds.id -> ds.read.where(col("study_id").isin(studyIds: _*))
       ) ++ Seq(
-      normalized_patient.id -> normalized_patient.read
-        .where(col("study_id").isin(studyIds: _*))
-        .where(col("security") =!= "R"),
+//      normalized_patient.id -> normalized_patient.read
+//        .where(col("study_id").isin(studyIds: _*))
+//        .where(col("security") =!= "R"),
       hpo_terms.id -> hpo_terms.read,
       mondo_terms.id -> mondo_terms.read,
       icd_terms.id -> icd_terms.read,
