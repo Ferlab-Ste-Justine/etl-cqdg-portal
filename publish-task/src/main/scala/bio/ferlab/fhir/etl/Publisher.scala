ea2648f478ce5a01c683616b235a7a0e262d8aee
Adrian <adipaul1981@yahoo.ca>
2025-07-15 11:52:51 -0400

    fix: CQDG-00 test publish

--- publish-task/src/main/scala/bio/ferlab/fhir/etl/Publisher.scala
+++ publish-task/src/main/scala/bio/ferlab/fhir/etl/Publisher.scala
@@ -47,7 +47,7 @@ object Publisher {
 
   def retrieveIndexesFromRegex(regex: String, fromQuery: String)(esNodes: String, esPort: String)
                               (implicit esHttpClient: EsHttpClient): Seq[String] = {
-    val httpRequest = new HttpGet(s"$esNodes:$esPort/_cat/$fromQuery?h=index")
+    val httpRequest = new HttpGet(s"$esNodes/_cat/$fromQuery?h=index")
 
     val (body, status) = esHttpClient.executeHttpRequest(httpRequest)
 
@@ -60,7 +60,7 @@ object Publisher {
   def updateAlias(alias: String, currentIndex: String, opType: String)(esNodes: String, esPort: String)
                  (implicit esHttpClient: EsHttpClient): Unit = {
     val query = s"""{\"actions\":[{\"$opType\":{\"index\":\"$currentIndex\",\"alias\":\"$alias\"}}]}"""
-    val httpRequest = new HttpPost(s"$esNodes:$esPort/_aliases")
+    val httpRequest = new HttpPost(s"$esNodes/_aliases")
 
     httpRequest.setEntity(new StringEntity(query))
     httpRequest.addHeader("Content-Type", "application/json")
