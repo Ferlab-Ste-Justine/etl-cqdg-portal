11036f9344298496fb4fdc8ae477cdfd2e9e8cae
Charles Morin <charles.mor@outlook.fr>
2026-01-16 14:27:25 -0500

    feat: CQDG-1331 updated pipeline to pipelines in prepare-index

--- prepare-index/src/test/scala/ParticipantCentricSpec.scala
+++ prepare-index/src/test/scala/ParticipantCentricSpec.scala
@@ -1,40 +1,65 @@
-import bio.ferlab.datalake.commons.config.{Configuration, ConfigurationLoader, SimpleConfiguration}
+import bio.ferlab.datalake.commons.config.{
+  Configuration,
+  ConfigurationLoader,
+  SimpleConfiguration
+}
 import bio.ferlab.datalake.spark3.loader.GenericLoader.read
 import bio.ferlab.fhir.etl.centricTypes.ParticipantCentric
 import model._
-import model.input.{BIOSPECIMEN_INPUT, CODE_SYSTEM_DISPLAY_INPUT, CODE_SYSTEM_INPUT_TEXT, SAMPLE_INPUT}
+import model.input.{
+  BIOSPECIMEN_INPUT,
+  CODE_SYSTEM_DISPLAY_INPUT,
+  CODE_SYSTEM_INPUT_TEXT,
+  SAMPLE_INPUT
+}
 import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.functions.col
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 import pureconfig.generic.auto._
 
-class ParticipantCentricSpec extends AnyFlatSpec with Matchers with WithSparkSession {
+class ParticipantCentricSpec
+    extends AnyFlatSpec
+    with Matchers
+    with WithSparkSession {
 
   import spark.implicits._
 
-  implicit val conf: Configuration = ConfigurationLoader.loadFromResources[SimpleConfiguration]("config/dev-cqdg.conf")
+  implicit val conf: Configuration =
+    ConfigurationLoader.loadFromResources[SimpleConfiguration](
+      "config/dev-cqdg.conf"
+    )
 
   "transform" should "prepare index participant_centric" in {
     val data: Map[String, DataFrame] = Map(
-      "simple_participant" -> Seq (
+      "simple_participant" -> Seq(
         SIMPLE_PARTICIPANT(`participant_id` = "P1"),
         SIMPLE_PARTICIPANT(`participant_id` = "P2")
       ).toDF(),
       "normalized_document_reference" -> Seq(
         DOCUMENTREFERENCE(`fhir_id` = "1", `participant_id` = "P1"),
-        DOCUMENTREFERENCE(`fhir_id` = "2", `participant_id` = "P2"),
+        DOCUMENTREFERENCE(`fhir_id` = "2", `participant_id` = "P2")
       ).toDF(),
       "normalized_biospecimen" -> Seq(
         BIOSPECIMEN_INPUT(
           `fhir_id` = "B1",
           `subject` = "P1",
-          `cancer_anatomic_location` = CODE_SYSTEM_INPUT_TEXT(`code` = "NCIT:C12434", `text` = Some("locationB1")),
-          `tumor_histological_type` = CODE_SYSTEM_INPUT_TEXT(`display`= Some("Missing - Not Provided"), `system`= "https://fhir.cqdg.ca/CodeSystem/cqdg-specimen-missing-codes", `code` = "Missing - Not provided", `text` = Some("histological_type5")),
-          `biospecimen_tissue_source` = CODE_SYSTEM_DISPLAY_INPUT(`code` = "NCIT:C12434"),
+          `cancer_anatomic_location` = CODE_SYSTEM_INPUT_TEXT(
+            `code` = "NCIT:C12434",
+            `text` = Some("locationB1")
+          ),
+          `tumor_histological_type` = CODE_SYSTEM_INPUT_TEXT(
+            `display` = Some("Missing - Not Provided"),
+            `system` =
+              "https://fhir.cqdg.ca/CodeSystem/cqdg-specimen-missing-codes",
+            `code` = "Missing - Not provided",
+            `text` = Some("histological_type5")
+          ),
+          `biospecimen_tissue_source` =
+            CODE_SYSTEM_DISPLAY_INPUT(`code` = "NCIT:C12434"),
           `cancer_biospecimen_type` = Some(CODEABLE(`code` = "NCIT:C12434"))
-    ),
-        BIOSPECIMEN_INPUT(`fhir_id` = "B2", `subject` = "P2"),
+        ),
+        BIOSPECIMEN_INPUT(`fhir_id` = "B2", `subject` = "P2")
       ).toDF(),
       "normalized_task" -> Seq(
         TASK(fhir_id = "1"),
@@ -45,18 +70,26 @@ class ParticipantCentricSpec extends AnyFlatSpec with Matchers with WithSparkSes
         SAMPLE_INPUT(fhir_id = "S2", `subject` = "P2", `parent` = "B2")
       ).toDF(),
       "es_index_study_centric" -> Seq(STUDY_CENTRIC()).toDF(),
-      "ncit_terms" -> read(getClass.getResource("/ncit_terms").toString, "Parquet", Map(), None, None),
+      "ncit_terms" -> read(
+        getClass.getResource("/ncit_terms").toString,
+        "Parquet",
+        Map(),
+        None,
+        None
+      )
     )
 
-    val output = new ParticipantCentric(List("SD_Z6MWD3H0"))(conf).transform(data)
+    val output =
+      new ParticipantCentric(List("SD_Z6MWD3H0"))(conf).transform(data)
 
     output.keys should contain("es_index_participant_centric")
 
-    val participant_centric = output("es_index_participant_centric").as[PARTICIPANT_CENTRIC].collect()
+    val participant_centric =
+      output("es_index_participant_centric").as[PARTICIPANT_CENTRIC].collect()
 
     participant_centric.find(_.`participant_id` == "P1") shouldBe Some(
       PARTICIPANT_CENTRIC(
-        `participant_id`= "P1",
+        `participant_id` = "P1",
         `biospecimens` = Seq(
           BIOSPECIMEN(
             `biospecimen_id` = "B1",
@@ -72,7 +105,7 @@ class ParticipantCentricSpec extends AnyFlatSpec with Matchers with WithSparkSes
               code = "Missing - Not provided",
               text = Some("histological_type5")
             )
-          ),
+          )
         ),
         `files` = Seq(
           FILE_WITH_BIOSPECIMEN(
@@ -81,8 +114,11 @@ class ParticipantCentricSpec extends AnyFlatSpec with Matchers with WithSparkSes
             `file_name` = Some("file5.json"),
             `file_format` = Some("TGZ"),
             `file_size` = Some(56.0),
-            `ferload_url` = Some("http://flerloadurl/outputPrefix/bc3aaa2a-63e4-4201-aec9-6b7b41a1e64a"),
-            `biospecimen_reference` = Seq("SAM0000001", "SAM0000002", "SAM0000003"),
+            `ferload_url` = Some(
+              "http://flerloadurl/outputPrefix/bc3aaa2a-63e4-4201-aec9-6b7b41a1e64a"
+            ),
+            `biospecimen_reference` =
+              Seq("SAM0000001", "SAM0000002", "SAM0000003"),
             `data_type` = Some("SSUP"),
             `dataset` = Some("Dataset1"),
             `biospecimens` = Seq(
@@ -102,10 +138,9 @@ class ParticipantCentricSpec extends AnyFlatSpec with Matchers with WithSparkSes
                   text = Some("histological_type5")
                 )
               )
-
             ),
             `sequencing_experiment` = Some(SEQUENCING_EXPERIMENT_SINGLE())
-          ),
+          )
         )
       )
     )
--- prepare-index/src/test/scala/model/SEQUENCING_EXPERIMENT_INPUT.scala
+++ prepare-index/src/test/scala/model/SEQUENCING_EXPERIMENT_INPUT.scala
@@ -1,30 +1,30 @@
 package model
 
 case class SEQUENCING_EXPERIMENT_SINGLE(
-                                  `owner`: String = "CQDG",
-                                  `experimental_strategy_1`: CODEABLE = CODEABLE("WXS", "wxs_display"),
-                                  `experimental_strategy`: String = "WXS", // FIXME remove this field after all studies are updated (replace _1)
-                                  `selection`: CODEABLE = CODEABLE("RR", "RR_display"),
-                                  `source`: CODEABLE = CODEABLE("TSC", "TSC_display"),
-                                  `protocol`: String = "protocol2",
-                                  `target_capture_kit`: String = "targetCaptureKit2",
-                                  `target_loci`: String = "targetedLoci2",
-                                  `pipeline`: String = "testPipeline",
-                                  `run_ids`: Seq[String] = Seq("runNameExample"),
-                                  `run_dates`: Seq[String] = Seq("2007-04-14"),
+    `owner`: String = "CQDG",
+    `experimental_strategy_1`: CODEABLE = CODEABLE("WXS", "wxs_display"),
+    `experimental_strategy`: String =
+      "WXS", // FIXME remove this field after all studies are updated (replace _1)
+    `selection`: CODEABLE = CODEABLE("RR", "RR_display"),
+    `source`: CODEABLE = CODEABLE("TSC", "TSC_display"),
+    `protocol`: String = "protocol2",
+    `target_capture_kit`: String = "targetCaptureKit2",
+    `target_loci`: String = "targetedLoci2",
+    `pipelines`: Seq[String] = Seq("testPipeline1", "testPipeline2"),
+    `run_ids`: Seq[String] = Seq("runNameExample"),
+    `run_dates`: Seq[String] = Seq("2007-04-14"),
 //                                  `is_paired_end`: Boolean = false,
-                                  `analysis_files`: Seq[ANALYSIS_FILE] = Seq(
-                                    ANALYSIS_FILE("Annotated-SNV", "12"),
-                                    ANALYSIS_FILE("Aligned-reads", "1"),
-                                    ANALYSIS_FILE("SNV", "2"),
-                                    ANALYSIS_FILE("Germline-CNV", "3"),
-                                    ANALYSIS_FILE("Germline-structural-variant", "4"),
-                                    ANALYSIS_FILE("Sequencing-data-supplement", "5"),
-                                  )
-                                )
+    `analysis_files`: Seq[ANALYSIS_FILE] = Seq(
+      ANALYSIS_FILE("Annotated-SNV", "12"),
+      ANALYSIS_FILE("Aligned-reads", "1"),
+      ANALYSIS_FILE("SNV", "2"),
+      ANALYSIS_FILE("Germline-CNV", "3"),
+      ANALYSIS_FILE("Germline-structural-variant", "4"),
+      ANALYSIS_FILE("Sequencing-data-supplement", "5")
+    )
+)
 
 case class TASK_SAMPLE(
-                        `lab_aliquot_ids`: Seq[String] = Seq("nanuq_sample_id"),
-                        `ldm_sample_id`: String = "S16523",
-                      )
-
+    `lab_aliquot_ids`: Seq[String] = Seq("nanuq_sample_id"),
+    `ldm_sample_id`: String = "S16523"
+)
